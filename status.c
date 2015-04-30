47f5ca83c635ad841371ee483e4a31456d07800c
gdibernardo <gabriele.dibernardo@me.com>
2015-04-30 02:16:01 +0200

    few enhancements in design

--- status.c
+++ status.c
@@ -925,6 +925,44 @@ void ieee80211_tx_status(struct ieee80211_hw *hw, struct sk_buff *skb)
 			local->dot11FailedCount++;
 	}
 
+    /* ABPS Gab */
+    if (skb)
+    {
+        struct sock *sk=skb->sk;
+        
+        /* *** ABPS ***
+         * this line should be modified when the data pass through the
+         * library functions to module ABPS
+         */
+        if (required_ip_local_error_notify(sk))
+        {
+            struct ieee80211_hdr *hdr = NULL;
+            int ret;
+            struct net_device *dev = skb->dev;
+            if(dev)
+            {
+                sdata = IEEE80211_DEV_TO_SUB_IF(dev);
+                if(sdata)
+                {
+                    hdr=(struct ieee80211_hdr *)skb->data;
+                    ret=ABPS_info_response(sk, hw, hdr, info, sdata);
+                    printk(KERN_DEBUG "*** ABPS *** ieee80211_tx_status:"
+                           " ABPS_info_response value %d \n", ret);
+                }
+                else
+                {
+                    printk(KERN_NOTICE "sdata is null in in tx_status");
+                }
+                sdata = NULL;
+            }
+            else
+            {
+                printk(KERN_NOTICE "dev field is null in skb in tx_status");
+            }
+        }
+    }
+
+    
 	if (ieee80211_is_nullfunc(fc) && ieee80211_has_pm(fc) &&
 	    (local->hw.flags & IEEE80211_HW_REPORTS_TX_ACK_STATUS) &&
 	    !(info->flags & IEEE80211_TX_CTL_INJECTED) &&
@@ -939,43 +977,6 @@ void ieee80211_tx_status(struct ieee80211_hw *hw, struct sk_buff *skb)
 
 	ieee80211_report_used_skb(local, skb, false);
     
-//    /* ABPS Gab */
-//    if (skb)
-//    {
-//        struct sock *sk=skb->sk;
-//        
-//        /* *** ABPS ***
-//         * this line should be modified when the data pass through the
-//         * library functions to module ABPS
-//         */
-//        if (required_ip_local_error_notify(sk))
-//        {
-//            struct ieee80211_hdr *hdr = NULL;
-//            int ret;
-//            struct net_device *dev = skb->dev;
-//            if(dev)
-//            {
-//                sdata = IEEE80211_DEV_TO_SUB_IF(dev);
-//                if(sdata)
-//                {
-//                    hdr=(struct ieee80211_hdr *)skb->data;
-//                    ret=ABPS_info_response(sk, hw, hdr, info, sdata);
-//                    printk(KERN_DEBUG "*** ABPS *** ieee80211_tx_status:"
-//                           " ABPS_info_response value %d \n", ret);
-//                }
-//                else
-//                {
-//                    printk(KERN_NOTICE "sdata is null in in tx_status");
-//                }
-//                sdata = NULL;
-//            }
-//            else
-//            {
-//                printk(KERN_NOTICE "dev field is null in skb in tx_status");
-//            }
-//        }
-//    }
-
 	/* this was a transmitted frame, but now we want to reuse it */
 	skb_orphan(skb);
 
