886d5b6d6be59c44600cbf7c87160ff784fadfc0
gdibernardo <gabriele.dibernardo@me.com>
2015-05-01 21:28:09 +0200

    printk

--- tx.c
+++ tx.c
@@ -1178,6 +1178,7 @@ ieee80211_tx_prepare(struct ieee80211_sub_if_data *sdata,
 		     struct ieee80211_tx_data *tx,
 		     struct sk_buff *skb)
 {
+    /* ABPS Gab */
     printk (KERN_NOTICE "Preparing tx and skb identifier is %d", ntohl(skb->sk_buff_identifier));
     
 	struct ieee80211_local *local = sdata->local;
@@ -1559,6 +1560,9 @@ static int ieee80211_skb_resize(struct ieee80211_sub_if_data *sdata,
 
 void ieee80211_xmit(struct ieee80211_sub_if_data *sdata, struct sk_buff *skb)
 {
+    /*  ABPS Gab  */
+    printk(KERN_NOTICE "xmit skb identifier value %d ", ntohl(skb->sk_buff_identifier));
+    
 	struct ieee80211_local *local = sdata->local;
 	struct ieee80211_tx_info *info = IEEE80211_SKB_CB(skb);
 	struct ieee80211_hdr *hdr = (struct ieee80211_hdr *) skb->data;
@@ -1574,6 +1578,8 @@ void ieee80211_xmit(struct ieee80211_sub_if_data *sdata, struct sk_buff *skb)
 	headroom = max_t(int, 0, headroom);
 
 	if (ieee80211_skb_resize(sdata, skb, headroom, may_encrypt)) {
+        /*ABPS Gab*/
+        printk(KERN_NOTICE "prepare for free txskb \n");
 		ieee80211_free_txskb(&local->hw, skb);
 		return;
 	}
