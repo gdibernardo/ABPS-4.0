324ce8a1d5f56b9a3676641a720139fe6a61f72d
gdibernardo <gabriele.dibernardo@me.com>
2015-05-05 00:16:01 +0200

    scripts up to date with directory

--- scripts/put_files.sh
+++ scripts/put_files.sh
@@ -11,11 +11,17 @@ $CP -vaa ../dev.c                 $1/net/core/dev.c
 $CP -vaa ../skbuff.h              $1/include/linux/skbuff.h
 $CP -vaa ../errqueue.h            $1/include/uapi/linux/errqueue.h
 $CP -vaa ../Makefile              $1/net/mac80211/Makefile
+$CP -vaa ../socket.c              $1/net/socket.c
+
 
 $CP -vaa ../ipv4/ip_output.c      $1/net/ipv4/ip_output.c
 $CP -vaa ../ipv4/udp.c            $1/net/ipv4/udp.c
 $CP -vaa ../ipv6/ip6_output.c     $1/net/ipv6/ip6_output.c
 $CP -vaa ../ipv6/udp.c            $1/net/ipv6/udp.c
 
-$CP -va ../ipv4/ip.h		  $1/include/net/ip.h
+$CP -va ../ipv4/ip.h              $1/include/net/ip.h
+$CP -va ../socket.h               $1/include/linux/socket.h
+
+
+
 $CP -va ../ipv4/ip_sockglue.c     $1/net/ipv4/ip_sockglue.c
