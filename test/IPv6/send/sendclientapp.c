d13ab9158e821ef943a15d8b34b5a1365de5ea77
gdibernardo <gabriele.dibernardo@me.com>
2015-05-10 19:38:17 +0200

    test

---
+++ test/IPv6/send/sendclientapp.c
@@ -0,0 +1,36 @@
+//
+//  sendclientapp.c
+//  
+//
+//  Created by Gabriele Di Bernardo on 10/05/15.
+//
+//
+
+#include <stdio.h>
+#include <string.h>
+#include "libsend.h"
+
+int main(int argc, char ** argv)
+{
+    if(argc < 3)
+    {
+        return 1;
+    }
+    
+    char *address = argv[1];
+    
+    int port = atoi(argv[2]);
+    
+    printf("Ready to alloc shared instance with parameters %s %d",address,port);
+    
+    
+    /* check for errror */
+    instantiate_ipv6_shared_instance_by_address_and_port(address, port);
+    char buffer[2000];
+    memset(buffer,0,2000);
+    strcpy(buffer,"hello from client app");
+    send_packet_with_message(buffer, 2000);
+    sleep(3);
+    receive_local_error_notify(void);
+    return 0;
+}
---
+++ test/IPv6/send/sendrecvUDP.h
@@ -0,0 +1,79 @@
+#ifndef __SENDRECVUDP_H__
+#define __SENDRECVUDP_H__
+
+/* sendrecvUDP.h */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/udp.h> /* SOL_UDP define. */
+#include <arpa/inet.h>
+#include <errno.h>
+
+#define SOCKET_ERROR   ((int)-1)
+
+#define SIZEBYTE 70000
+
+typedef unsigned int u32;
+
+
+#define HAVE_IP_RECVERR 1
+
+#if HAVE_IP_RECVERR
+
+#include <sys/uio.h>
+#else
+#warning "HAVE_IP_RECVERR not defined"
+		   printf ("setsockopt() IP_PROTO IP_RECVERR not defined\n");
+#endif
+
+typedef struct struct_errormessage
+{
+    union name
+    {
+        struct sockaddr_in6	ipv6_name;
+        struct sockaddr_in ipv4_name;
+    };
+    
+    unsigned int namelen;
+    
+    struct cmsghdr *c;
+    
+    struct sock_extended_err *ee;
+    
+    char control[512];
+    
+    int	len_control;
+    
+    char errmsg[64 + 768];
+    
+    int	len_errmsg;
+    
+    struct msghdr msg[1];
+    
+    int	lenrecv;
+    int	myerrno;
+    
+} ErrMsg;
+
+
+int ipv4_sendmsg_udp(int udp_file_descriptor, unsigned char *buffer, int length, struct sockaddr_in destination_address, uint32_t *pointer_for_identifier);
+
+int ipv6_sendmsg_udp(int udp_file_descriptor, unsigned char *buffer, int length, struct sockaddr_in6 destination_address, uint32_t *pointer_for_identifier);
+
+
+ErrMsg	*allocinit_ErrMsg( void );
+
+
+int ipv6_receive_error_message_no_wait(int descriptor, ErrMsg *em);
+
+
+
+#endif /* __SENDRECVUDP_H__ */
+
+
