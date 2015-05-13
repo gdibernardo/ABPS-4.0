7625239cfb2cf8ced207c7823b841c1101433048
gdibernardo <gabriele.dibernardo@me.com>
2015-05-13 17:59:32 +0200

    one hundred messages

--- test/IPv6/send/sendrecvUDP.c
+++ test/IPv6/send/sendrecvUDP.c
@@ -306,7 +306,7 @@ int ipv6_receive_error_message_wait(int descriptor, ErrMsg *em)
         em->msg->msg_control = em->control;
         em->msg->msg_controllen = sizeof(em->control);
         
-        return_value = recvmsg(descriptor, em->msg, MSG_ERRQUEUE);
+        return_value = recvmsg(descriptor, em->msg, MSG_ERRQUEUE | MSG_WAITALL);
         em->myerrno=errno;
         
     } while ((return_value < 0) && (em->myerrno == EINTR));
