e73bddbc70302ab16b834afe808519b858147f57
gdibernardo <gabriele.dibernardo@me.com>
2015-07-02 19:55:00 +0200

    extension

--- test/IPv6/fragmentation test/ipv6fragmentationapp.c
+++ test/IPv6/fragmentation test/ipv6fragmentationapp.c
@@ -1,6 +1,6 @@
 //
 //  ipv6fragmentationapp.c
-//  
+//
 //
 //  Created by Gabriele Di Bernardo on 02/07/15.
 //
@@ -34,12 +34,12 @@ int main(int argc, char ** argv)
     int port = atoi(argv[2]);
     
     int number_of_packets = 0;
-
+    
     int stop_sending = 0;
     
     /* check for error */
     int file_descriptor = instantiate_ipv6_shared_instance_by_address_and_port(address, port);
-
+    
     while (1)
     {
         if(!stop_sending)
@@ -67,29 +67,72 @@ int main(int argc, char ** argv)
             
             number_of_packets++;
             if(number_of_packets == NUMBER_OF_PACKETS)
-            {
                 stop_sending = 1
                 
                 struct msghdr message;
-                struct cmsghdr *cmsg;
-                
-                struct sock_extended_err *first_hop_transmission_notification;
-                
-                int return_value;
-                
+            struct cmsghdr *cmsg;
+            
+            struct sock_extended_err *first_hop_transmission_notification;
+            
+            int return_value;
+            do
+            {
                 return_value = recvmsg(socket, &message, MSG_ERRQUEUE);
-                if(return_value > 0)
-                {
                 
+            } while ((return_value < 0) && (em->myerrno == EINTR));
+            
+            if(return_value < 0)
+                continue;
+            
+            for(cmsg = CMSG_FIRSTHDR(message); cmsg; cmsg = CMSG_NXTHDR(message, cmsg))
+            {
+                /* IPv6 socket */
+                
+                if((cmsg->cmsg_level == IPPROTO_IPV6) && (cmsg->cmsg_type == IPV6_RECVERR))
+                {
+                    
+                    first_hop_transmission_notification = (struct sock_extended_err *) CMSG_DATA(cmsg);
+                    
+                    
+                    switch (first_hop_transmission_notification->ee_origin)
+                    {
+                            
+                            /* new origin type introduced  */
+                        case SO_EE_ORIGIN_LOCAL_NOTIFY:
+                        {
+                            if(first_hop_transmission_notification->ee_errno == 0)
+                            {
+                                uint32_t identifier = get_ted_identfier(first_hop_transmission_notification);
+                                
+                                uint8_t retry_count = get_ted_retry_count(first_hop_transmission_notification);
+                                
+                                printf("Received notification for packet %" PRIu32 " \n", identifier);
+                                
+                                uint8_t acked = get_ted_status(first_hop_transmission_notification);
+                                
+                                if(acked)
+                                {
+                                    printf("Your message with identifier %" PRIu32 " has been delivered to the first hop!", identifier);
+                                }
+                                else
+                                {
+                                    printf("Ups.. something went wrong! Your message with identifier %" PRIu32" has been lost. \n", identifier);
+                                }
+                            }
+                        }
+                        default:
+                        {
+                            if(error_message->ee->ee_errno != 0)
+                            {
+                                printf(strerror(first_hop_transmission_notification->ee_errno));
+                            }
+                            break;
+                        }
+                    }
+                    
                 }
             }
-            
-            
         }
         
-        
-        
-    }
-    
-    return 0;
+        return 0;
 }
