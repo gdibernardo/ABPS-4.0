60759d4039de9825bfced6f17c656d7a07797c0e
gdibernardo <gabriele.dibernardo@me.com>
2015-07-02 21:01:09 +0200

    extension

--- test/IPv6/fragmentation test/ipv6fragmentationapp.c
+++ test/IPv6/fragmentation test/ipv6fragmentationapp.c
@@ -67,77 +67,85 @@ int main(int argc, char ** argv)
             
             number_of_packets++;
         }
-            if(number_of_packets == NUMBER_OF_PACKETS)
-                stop_sending = 1;
-                
-            struct msghdr message[1];
-            struct cmsghdr *cmsg;
-            
-            struct sock_extended_err *first_hop_transmission_notification;
-            
-            int return_value, current_errno;
-            printf("read \n");
-            do
-            {
-                return_value = recvmsg(file_descriptor, message, MSG_ERRQUEUE|MSG_DONTWAIT);
-                
-                current_errno = errno;
-                
-            } while ((return_value < 0) && (current_errno == EINTR));
+        if(number_of_packets == NUMBER_OF_PACKETS)
+            stop_sending = 1;
         
-            printf(strerror(errno));
-            if(return_value < 0)
-                continue;
+        struct iovec iov[1]
+        struct msghdr message[1];
+        struct cmsghdr *cmsg;
+        
+        message->msg_iov = iov;
+        message->msg_iovlen = 1;
+        
+        message->msg_control = cmsg;
+        
+        message->msg_controllen = sizeof(cmsg);
+        
+        struct sock_extended_err *first_hop_transmission_notification;
             
-            for(cmsg = CMSG_FIRSTHDR(message); cmsg; cmsg = CMSG_NXTHDR(message, cmsg))
-            {
-                /* IPv6 socket */
+        int return_value, current_errno;
+        printf("read \n");
+        do
+        {
+            return_value = recvmsg(file_descriptor, message, MSG_ERRQUEUE|MSG_DONTWAIT);
+            
+            current_errno = errno;
                 
-                if((cmsg->cmsg_level == IPPROTO_IPV6) && (cmsg->cmsg_type == IPV6_RECVERR))
+        } while ((return_value < 0) && (current_errno == EINTR));
+        
+        printf(strerror(errno));
+        if(return_value < 0)
+            continue;
+        
+        for(cmsg = CMSG_FIRSTHDR(message); cmsg; cmsg = CMSG_NXTHDR(message, cmsg))
+        {
+            /* IPv6 socket */
+            
+            if((cmsg->cmsg_level == IPPROTO_IPV6) && (cmsg->cmsg_type == IPV6_RECVERR))
+            {
+                
+                first_hop_transmission_notification = (struct sock_extended_err *) CMSG_DATA(cmsg);
+            
+                
+                switch (first_hop_transmission_notification->ee_origin)
                 {
-                    
-                    first_hop_transmission_notification = (struct sock_extended_err *) CMSG_DATA(cmsg);
-                    
-                    
-                    switch (first_hop_transmission_notification->ee_origin)
+                
+                        /* new origin type introduced  */
+                    case SO_EE_ORIGIN_LOCAL_NOTIFY:
                     {
+                        if(first_hop_transmission_notification->ee_errno == 0)
+                        {
+                            uint32_t identifier = get_ted_identfier(first_hop_transmission_notification);
                             
-                            /* new origin type introduced  */
-                        case SO_EE_ORIGIN_LOCAL_NOTIFY:
-                        {
-                            if(first_hop_transmission_notification->ee_errno == 0)
+                            uint8_t retry_count = get_ted_retry_count(first_hop_transmission_notification);
+                            
+                            printf("Received notification for packet %" PRIu32 " \n", identifier);
+                            
+                            uint8_t acked = get_ted_status(first_hop_transmission_notification);
+                            
+                            if(acked)
                             {
-                                uint32_t identifier = get_ted_identfier(first_hop_transmission_notification);
-                                
-                                uint8_t retry_count = get_ted_retry_count(first_hop_transmission_notification);
-                                
-                                printf("Received notification for packet %" PRIu32 " \n", identifier);
-                                
-                                uint8_t acked = get_ted_status(first_hop_transmission_notification);
-                                
-                                if(acked)
-                                {
-                                    printf("Your message with identifier %" PRIu32 " has been delivered to the first hop!", identifier);
-                                }
-                                else
-                                {
-                                    printf("Ups.. something went wrong! Your message with identifier %" PRIu32" has been lost. \n", identifier);
-                                }
+                                printf("Your message with identifier %" PRIu32 " has been delivered to the first hop!", identifier);
+                            }
+                            else
+                            {
+                                printf("Ups.. something went wrong! Your message with identifier %" PRIu32" has been lost. \n", identifier);
                             }
                         }
-                        default:
-                        {
-                            if(first_hop_transmission_notification->ee_errno != 0)
-                            {
-                                printf(strerror(first_hop_transmission_notification->ee_errno));
-                            }
-                            break;
-                        }
-                    }
-                    
                 }
+                    default:
+                    {
+                        if(first_hop_transmission_notification->ee_errno != 0)
+                        {
+                            printf(strerror(first_hop_transmission_notification->ee_errno));
+                        }
+                        break;
+                    }
+                }
+                
             }
         }
-        
-        return 0;
+    }
+    
+    return 0;
 }
